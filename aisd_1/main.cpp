7662761529a11a412a3189380653e93bd18d3a95
wardonne <Wardonne@yandex.ru>
2023-12-06 01:11:22 +0400

    Initial Commit

---
+++ aisd_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  aisd_1
+//
+//  Created by Роман Казаев on 05.12.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
